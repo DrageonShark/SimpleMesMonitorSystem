161e056924dd479da4e5868dc5e51e9c0df852e3
SimpleMesSystem <D2159505545@outlook.com>
2025-12-06 23:34:44 +0800

    完成IDisposable的接口设置

--- Services/Device/IDisposable.cs
+++ Services/Device/IDisposable.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace WPF9SimpleMesMonitorSystem.Services.Device
 {
-    internal interface IDisposable
+    public interface IDisposable
     {
+        /// <summary>
+        /// 连接设备 (自动识别 TCP 或 RTU)
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> ConnectAsync();
+
+        /// <summary>
+        /// 读取数据核心逻辑
+        /// </summary>
+        /// <returns></returns>
+        Task ReadDataAsync();
+
+        /// <summary>
+        /// 关闭连接
+        /// </summary>
+        void Dispose();
     }
 }
