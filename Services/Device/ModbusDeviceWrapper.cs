09b2eade2d51c4881a0a8aead2741fdec495db20
SimpleMesSystem <D2159505545@outlook.com>
2025-12-07 11:33:06 +0800

    test

--- Services/Device/ModbusDeviceWrapper.cs
+++ Services/Device/ModbusDeviceWrapper.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using NModbus;
 
 namespace WPF9SimpleMesMonitorSystem.Services.Device
 {
     public class ModbusDeviceWrapper:IDisposable
     {
+        public Models.Device DeviceInfo { get; private set; }
+
+        private IModbusMaster _master;
+        private TcpClient _tcpClient;
+        private SerialPort _serialPort;
+
         public async Task<bool> ConnectAsync()
         {
             throw new NotImplementedException();
